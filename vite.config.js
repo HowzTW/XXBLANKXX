0c06a4ebfa30109d904b0401918c0d839b95adbd
HowzTW <34091465+HowzTW@users.noreply.github.com>
2024-04-26 09:13:15 +0800

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,67 @@
 import { fileURLToPath, URL } from 'node:url'
-
+import { VitePWA } from 'vite-plugin-pwa'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "./",
+  publicPath: '/XXBLANKXX/',
   plugins: [
     vue(),
+    VitePWA({
+      manifest: {
+        name: 'XXBLANKXX',
+        short_name: 'XXBLANKXX',
+        description: 'My Awesome App description XXBLANKXX',
+        theme_color: '#ffffff',
+        icons: [		//增加 icon 圖檔，注意路徑和像素正確
+        {
+          "src": "images/icon-128.png",
+          "sizes": "128x128",
+          "type": "image/png",
+          "purpose": "maskable"
+      },
+      {
+          "src": "images/icon-144.png",
+          "sizes": "144x144",
+          "type": "image/png",
+          "purpose": "maskable"
+      },
+      {
+          "src": "images/icon-152.png",
+          "sizes": "152x152",
+          "type": "image/png",
+          "purpose": "maskable"
+      },
+      {
+          "src": "images/icon-192.png",
+          "sizes": "192x192",
+          "type": "image/png",
+          "purpose": "maskable"
+      },
+      {
+          "src": "images/icon-256.png",
+          "sizes": "256x256",
+          "type": "image/png",
+          "purpose": "maskable"
+      },
+      {
+          "src": "images/icon-512.png",
+          "sizes": "512x512",
+          "type": "image/png",
+          "purpose": "maskable"
+      }
+      ]
+      },
+      registerType: 'autoUpdate',
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}'],		//緩存相關靜態資源资源
+      },
+      devOptions: {
+        enabled: true
+      }
+    })
   ],
   resolve: {
     alias: {
